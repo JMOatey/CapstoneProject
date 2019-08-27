c3a2ab56c4a76b017ec9867773d50c81f854311d
JMOatey <jackoman100@gmail.com>
2019-08-27 15:37:41 -0500

    I CAN MAKE A BOARD

--- FyreEmblemCapstone/Assets/Scripts/BoardManager.cs
+++ FyreEmblemCapstone/Assets/Scripts/BoardManager.cs
@@ -4,68 +4,39 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour 
 {
+	public int Width;
+	public int Height;
+	public GameObject Cube1;
+	public GameObject Cube2;
 
-	private const float TILE_SIZE = 1.0f;
-	private const float TILE_OFFSET = 0.5f;
-
-	private int selectionX = -1;
-	private int selectionY = -1;
-
-	public List<GameObject> chessmanPrefabs;
-	private List<GameObject> activeChessman;
+	// Use this for initialization
+	void Start()
+	{
+		CreateBoard();
+	}
 	
-	// Update is called once per frame
-	void Update () {
-		DrawChessBoard();
-		UpdateSelection();
+	private void CreateCube(GameObject cube, int posX, int posY, int posZ)
+	{
+		GameObject.Instantiate(cube, new Vector3(posX, posY, posZ), Quaternion.identity);
 	}
 
-	private void DrawChessBoard()
+	private void CreateBoard()
 	{
-		Vector3 widthLine = Vector3.right * 8;
-		Vector3 heightLine = Vector3.forward * 8;
-
-		for(int i=0; i<=8; i++)
+		bool isWhite = true;
+		for(int i = 0; i < this.Width; i++)
 		{
-			Vector3 start = Vector3.forward * i;
-			Debug.DrawLine(start, start + widthLine);
-			for(int j=0; j<=8; j++)
+			for(int j = 0; j < this.Height; j++)
 			{
-				start = Vector3.right * j;
-				Debug.DrawLine(start, start + heightLine);
+				if(isWhite)
+				{
+					CreateCube(Cube1, i, 0, j);
+				} else 
+				{
+					CreateCube(Cube2, i, 0, j);
+				}
+				isWhite = !isWhite;
 			}
-		}
-
-		if(selectionX >= 0 && selectionY >= 0)
-		{
-			Debug.DrawLine(
-				Vector3.forward * selectionY + Vector3.right * selectionX,
-				Vector3.forward * (selectionY + 1) + Vector3.right * (selectionX + 1)
-			);
-			Debug.DrawLine(
-				Vector3.forward * (selectionY + 1) + Vector3.right * selectionX,
-				Vector3.forward * selectionY + Vector3.right * (selectionX + 1)
-			);
-		}
-	}
-
-	private void UpdateSelection()
-	{
-		if (!Camera.main)
-		{
-			return;
-		}
-
-		RaycastHit hit;
-		if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
-		{
-			selectionX = (int)hit.point.x;
-			selectionY = (int)hit.point.z;
-		}
-		else
-		{
-			selectionX = -1;
-			selectionY = -1;
+			isWhite = !isWhite;
 		}
 	}
 }
